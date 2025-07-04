47b0af02b9278b4e33f6d74178d08288f3f02812
Leo <leo@vitex.asia>
2025-07-04 13:59:25 +0700

    fix form

--- logo-page/src/app/admin/danhmuc/LegoCategoryTable.tsx
+++ logo-page/src/app/admin/danhmuc/LegoCategoryTable.tsx
@@ -25,7 +25,7 @@ const LegoCategoryTable: React.FC<LegoCategoryTableProps> = ({
   onDelete,
 }) => {
   return (
-    <div className="border-3 border-blue-500 rounded-2xl overflow-x-auto">
+    <div className="border-3 border-blue-500 rounded-2xl mt-3 overflow-x-auto shadow-2xl shadow-blue-500/20">
       <Table>
         <TableHeader className="bg-blue-500">
           <TableRow className="font-bold">
